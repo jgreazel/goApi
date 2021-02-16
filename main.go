83fba097a12dcfb15f83019e653b4845c9f60f63
Jon <jdgreazel@gmail.com>
2021-02-15 23:06:10 -0600

    get methods work

--- main.go
+++ main.go
@@ -1,4 +1,90 @@
 package main
 
+import (
+	// "encoding/json"
+	"encoding/json"
+	"log"
+
+	"github.com/gorilla/mux"
+
+	// "math/rand"
+	"net/http"
+	// "strconv"
+)
+
+// Book struct (model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// init books var as a slice Book struct
+var books []Book
+
+// get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// get single book by id
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //get params
+	// loop through books and find correct id
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+// create a new book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// update existing book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// delete a book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
+	// Init router
+	router := mux.NewRouter()
+
+	//mock data
+	books = append(books, Book{
+		ID: "1", Isbn: "52634", Title: "Harry Potter", Author: &Author{
+			Firstname: "J.K.", Lastname: "Rowling"}})
+	books = append(books, Book{
+		ID: "2", Isbn: "98794", Title: "Bible", Author: &Author{
+			Firstname: "Jesus", Lastname: "Christ"}})
+	books = append(books, Book{
+		ID: "3", Isbn: "35400", Title: "Girl Wash Your Face", Author: &Author{
+			Firstname: "Dumb", Lastname: "B."}})
+
+	// route handlers / endpoints
+	router.HandleFunc("/api/books", getBooks).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/api/books", createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
